ca4548ffed237f993626e02be46cc1c9656b2375
Hectorsxo <hectorcrebu@gmail.com>
2025-09-30 21:46:19 +0200

    Creada la clase Iniciales (act 9).

---
+++ src/main/java/com/ieschabas/Iniciales.java
@@ -0,0 +1,43 @@
+package com.ieschabas;
+/**
+ * Programa que pide al usuario su nombre y primer apellido y muestra sus iniciales en mayúscula, separadas por punto.
+ * @author Héctor Crespo
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class Iniciales {
+    public static void main(String[] args) {
+
+        /**
+         * Creación del objeto Scanner.
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Pedir el nombre y apellido al usuario.
+         */
+        System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Introduce tu primer apellido: ");
+        String apellido = sc.nextLine();
+
+
+        /**
+         * Obtener la primera letra de cada uno y convertirla a mayúscula
+         */
+        char inicialNombre = Character.toUpperCase(nombre.charAt(0));
+        char inicialApellido = Character.toUpperCase(apellido.charAt(0));
+
+        /**
+         * Mostrar por pantalla las iniciales separadas por punto.
+         */
+
+        System.out.println("Iniciales: " + inicialNombre + "." + inicialApellido + ".");
+
+        sc.close();
+    }
+}
+
