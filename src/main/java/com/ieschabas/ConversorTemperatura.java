be3fd0fdd654b1d1e5df0d1c7d31274fae54091c
Hectorsxo <hectorcrebu@gmail.com>
2025-09-30 20:52:36 +0200

    Creada la clase ConversorTemperatura (act 4).

--- src/main/java/com/ieschabas/ConversorTemperatura.java
+++ src/main/java/com/ieschabas/ConversorTemperatura.java
@@ -1,7 +1,9 @@
 package com.ieschabas;
 
 /**
- * Programa que pida una temperatura en grados Celsius y muestre su equivalente en Fahrenheit y en Kelvin.
+ * Programa que pide una temperatura en grados Celsius y muestre su equivalente en Fahrenheit y en Kelvin.
+ * @author Héctor Crespo
+ * @versiom 1.0
  */
 
 import java.util.Scanner;
@@ -10,15 +12,21 @@ public class ConversorTemperatura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Pedir la temperatura en Celsius
+        /**
+         * Se pide la temperatura en celsius.
+         */
         System.out.print("Introduce la temperatura en Celsius: ");
         double celsius = sc.nextDouble();
 
-        // Conversión a Fahrenheit y Kelvin
+        /**
+         * Conversión de fahrenheit a kelvin.
+         */
         double fahrenheit = (celsius * 9 / 5) + 32;
         double kelvin = celsius + 273.15;
 
-        // Mostrar resultados
+        /**
+         * Muestra el resultado en consola.
+         */
         System.out.println("Fahrenheit: " + fahrenheit);
         System.out.println("Kelvin: " + kelvin);
 
