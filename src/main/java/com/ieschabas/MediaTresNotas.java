ebd72d22c8cfaa7d2ceaff4b69daa89be951a1b5
Hectorsxo <hectorcrebu@gmail.com>
2025-09-30 20:45:44 +0200

    Creada la clase MediaTresNotas (act 3).

---
+++ src/main/java/com/ieschabas/MediaTresNotas.java
@@ -0,0 +1,40 @@
+package com.ieschabas;
+
+import java.util.Scanner;
+
+public class MediaTresNotas {
+    public static void main(String[] args) {
+
+        /**
+         * Creación del objeto Scanner
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Pedir las notas al usuario
+         */
+        System.out.print("Introduce la primera nota: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.print("Introduce la segunda nota: ");
+        double nota2 = sc.nextDouble();
+
+        System.out.print("Introduce la tercera nota: ");
+        double nota3 = sc.nextDouble();
+
+        /**
+         * Calculo de la nota media
+         */
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        /**
+         * Mostrar el resultado
+         */
+        System.out.println("Media: " + media);
+
+
+
+        sc.close();
+    }
+}
+
