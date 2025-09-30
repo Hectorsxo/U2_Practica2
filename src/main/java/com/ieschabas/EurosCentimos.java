f0cc6d1dc9b1a05129261a85983a811f57f72945
Hectorsxo <hectorcrebu@gmail.com>
2025-09-30 21:39:41 +0200

    Creada la clase EurosCentimos (act 8).

---
+++ src/main/java/com/ieschabas/EurosCentimos.java
@@ -0,0 +1,44 @@
+package com.ieschabas;
+/**
+ * Programa que pide un precio en euros con céntimos (por ejemplo, 12.34) y muestra por separado la parte entera (euros) y los céntimos.
+ * @author Héctor Crespo
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class EurosCentimos {
+    public static void main(String[] args) {
+
+        /**
+         * Creación del objeto Scanner.
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Pedir al usuario el precio en euros con decimales
+         */
+        System.out.print("Introduce un precio en euros: ");
+        double precio = sc.nextDouble();
+
+        /**
+         * Obtener la parte entera (euros)
+         */
+
+        int euros = (int) precio;
+
+        /**
+         * Obtener los céntimos.
+         * Multiplicamos por 100 y restamos los euros ya convertidos a céntimos.
+         */
+        int centimos = (int) Math.round((precio - euros) * 100);
+
+        /**
+         * Mostrar el resultado
+         */
+        System.out.println("Euros: " + euros);
+        System.out.println("Céntimos: " + centimos);
+
+        sc.close();
+    }
+}
